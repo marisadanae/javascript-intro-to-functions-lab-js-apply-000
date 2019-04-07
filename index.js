4771208eb7c204809c9190e665d2a8b4294b4795
danaerodriguez96 <danaerodriguez96@gmail.com>
2019-04-07 02:45:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
-  function shout(string) {
-    return string.toUpperCase();
+function shout(string) {
+    return string.toUpperCase()
   }
 
 function whisper(string) {
-    return string.toLowerCase();
+    return string.toLowerCase()
   }
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function ('sayHiToGrandma(string') {
+
+  console.log(sayHiToGrandma('toLowerCase'()
+  .toEqual("I can't hear you!'string")
+
+
+function ('sayHiToGrandma(string') {
+
+console.log(sayHiToGrandma()
+.toEqual("YES INDEED!")
+
+
+function ('sayHiToGrandma(string') {
+
+console.log(sayHiToGrandma('Iloveyou,Grandma'()
+.toEqual("I love you,too")
+};
